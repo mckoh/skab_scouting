de8fb24581af899a352b88dd4204262fbe553a42
kohleggermichael <michael.kohlegger@fh-kufstein.ac.at>
2023-10-31 11:04:32 +0100

    changed snips shape to numpy.array for shape

--- skab_factory/preparation_engine.py
+++ skab_factory/preparation_engine.py
@@ -5,6 +5,7 @@ Date: October 2023
 """
 
 
+from numpy import array, append
 from .functions import load_data_from_dir
 
 
@@ -43,6 +44,8 @@ class PreparationEngine:
 
         self._X, self._y = None, None
         self._X_snips, self._y_snips = [], []
+        self._window_size = None
+        self._shift = None
 
     def __split_vertically(self, x_cols, y_cols):
         """Lets you select the columns that are used as machine learning input
@@ -66,13 +69,17 @@ class PreparationEngine:
         xv = self._X.values
         yv = self._y.values
 
+        self._window_size = window_size
+        self._shift = shift
+
         for i in range(0, len(xv)-window_size, shift):
             self._X_snips.append(xv[i:i+window_size])
             self._y_snips.append(yv[i+window_size])
 
-        print(f"I have created {len(self._X_snips)} snippets.")
+        self._X_snips = array(self._X_snips)
+        self._y_snips = array(self._y_snips)
 
-    def split_data(self, x_cols=X_COLUMNS, y_cols=Y_COLUMNS, window_size=10, shift=10):
+    def split_data(self, x_cols=X_COLUMNS, y_cols=Y_COLUMNS, window_size=10, shift=1):
         """Lets you perform a vertical and a horizontal split in one go
 
         This function is necessary, as it is common to first do a vertical
@@ -87,3 +94,38 @@ class PreparationEngine:
 
         self.__split_vertically(x_cols, y_cols)
         self.__split_horizontally(window_size, shift)
+
+    # GETTER FUNCTIONS
+
+    def get_data(self, return_xy=False):
+        """Lets you return your data.
+
+        :param return_xy: If true, method returns data as x an y
+        :return: Data as pandas.DataFrame
+        """
+        if return_xy:
+            return self._X, self._y
+        return self._data
+
+    def get_snippets(self):
+        """Lets you return your data as time snippets.
+
+        :return: Data as numpy.array
+        """
+        return self._X_snips, self._y_snips
+
+    def get_ts_params(self):
+        """Lets you return the construction parameters of your ts snips.
+
+        :return: parameters as tuple (window size, shift)
+        """
+        return self._window_size, self._shift
+
+    def get_snips_shape(self):
+        """Lets you return the shape of your snippets data.
+
+        :return: Shapes as tuple (X.shape, y.shape)
+        """
+        return self._X_snips.shape, self._y_snips.shape
+
+    #TODO: Data Visualization
